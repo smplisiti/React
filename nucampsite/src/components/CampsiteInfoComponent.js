9d1a388ac080989ce251ce504b561485c0f54b97
Heather <smplisiti@gmail.com>
2020-05-30 17:24:53 -1000

    task 3

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React,{ Component } from "react"
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import {formatDate, toSentenceCase} from "../shared/helpers"
 
 export default class CampsiteInfo extends Component {
   
@@ -16,9 +17,36 @@ export default class CampsiteInfo extends Component {
       </div>
     );
   }
-    render() {
-          const {campsite} = this.props
-         return campsite ? <div className="row">{this.renderCampsite(this.props.campsite)}</div> : <div />
-    }
-}
 
+  renderComments(comments) {
+    return comments ? (
+      <div className="col-md-5 m-1">
+        <h4>Comments</h4>
+        <div>
+          {comments.map(({ text, author, date, id }) => (
+            <div key={id}>
+              <p>{toSentenceCase(text)}</p>
+              <p>
+                -- {author} {formatDate(date)}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    ) : (
+      <div />
+    );
+  }
+  render() {
+    const { campsite } = this.props;
+    return campsite ? (
+      <div className="row">
+        {this.renderCampsite(campsite)} {this.renderComments(campsite.comments)}
+      </div>
+    ) : (
+      <div />
+    );
+  }
+}
+ 
+  
