257164502b18f8d9346fda03217baec419f711fa
Heather <smplisiti@gmail.com>
2020-05-30 16:57:33 -1000

    task 2

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,9 +1,24 @@
 import React,{ Component } from "react"
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 export default class CampsiteInfo extends Component {
   
+  renderCampsite({image, name, description}) {
+    return (
+      <div className="col-md-5 m-1">
+      <Card>
+          <CardImg top src={image} alt={name} />
+          <CardBody>
+            <CardTitle>{name}</CardTitle>
+            <CardText>{description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
     render() {
-        return this.props.campsite ? <div className="row" /> : <div />
+          const {campsite} = this.props
+         return campsite ? <div className="row">{this.renderCampsite(this.props.campsite)}</div> : <div />
     }
 }
 
